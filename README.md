318818ecb32e4f6fb342657d259f7699efee0f26
ost960102 <ostaxi1996@gmail.com>
2020-11-06 16:41:34 +0900

    오승택-1106수업

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # 오승택 201840123
 ## movie_app_2020
 React JS Fundamentlas Course 2020
+## 11월 06일
+>1. 언더바 두개(__)로 작명하는 방법은 잘못된 방법이다.
+>2. css는 kebab(ex.-good)을 사용(개발자의 가이드라인)
+
+## 10월 30일
+>1. 오류 문제는 App.js의 39번째 줄에 poster={movie.medium_cover_image} medium을 midium으로 잘못쳐 오탈자 때문에 오류가 난 것 같습니다. 
+
+## 10월 16일
+>1. constructor() 함수 → constructor 실행 후 render 실행
+>2. componentDidMount() 함수 → render 실행후 componentDidMount 실행
+>3. componentDidUpdate() 함수 → 화면이 새로 그려지게 되면 실행
+>4. componentDidUnmount() 함수
+>5. App 컴포넌트 비우기(영화 앱 만들기 워밍업)
+>6. 데이터 로딩 상태 표시해주기
+
+>* isLoading state 특징
+>1. 앱이 실행(mount)되는 초기에는 무조건 로딩 상태이기 때문에 값은 true이다.
+>2. 삼항연산자로 로딩 상태를 알려주는 문장을 만들 수 있다.
+>3. 강제로 false로 바꿔서 로딩 현상을 구현할 수 있다.
+>4. 영화 API 사용해보기
+
+>* API 특징
+>1. 특정 주소를 입력하면 그 주소에 맞는 결과를 보내준다.
+>2. 조건도 붙일 수 있도록 제공한다.
+>3. API를 사용하려면 axios를 설치 후 import하여야 한다. 이때, componentDidUnmount() 함수에서 axios.get() 함수의 인자에 URL을 전달하여 API를 호출한다.
+
+>* async, await
+>1. 자바스크립트에서 시간이 필요하다는 것을 알리기 위해서 사용되는 키워드이다.
+>2. 시간이 필요하다는 것을 알리려면 async를 () 앞에 붙이고, 실제 시간이 필요한 대상인 axios.get() 함수에는 await를 붙인다.
+
 ## 10월07일
 >1. rating에서 isRequired는 있어도되고 없어도 된다(평점을 안주는 사람도 있기때문에).
 >2. 전달되는 경우 자료형은 반드시 number이어야 한다.
--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
+  };
+  
+  getMovies = async () =>{
+    const {
+      data: {
+        data: {movies},
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    // 영화 데이터 로딩
+    this.getMovies();
   }
 
-  add   = () => {
-    this.setState(current => ({
-      count: current.count +1,
-    }));
-  };
-  minus = () => {
-    this.setState(current => ({
-      count: current.count -1
-    }));
-  };
-  render(){
-    return( 
-    <div>
-      <h1>I'm a class component {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>&nbsp;&nbsp;&nbsp;
-      <button onClick={this.minus}>Minus</button>
-    </div>
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? ( 
+        <div className="loader"><span className="loder__text">'Loading...'</span></div>
+        ) 
+        : (
+          <div className="movie">
+          { movies.map((movie) => {
+          return (
+            <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+            />
+          )})}
+          </div>
+        )}
+        </section>
     );
-  }
-} //클래스 컴포넌트의 기본 골격
+  } 
+}
 
 export default App;
